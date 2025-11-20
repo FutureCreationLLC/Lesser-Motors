fdf10fa63714ba7cfc58b6f1e079123aaf4c4589
Ransford Essel <ransfordessel345@gmail.com>
2025-11-20 13:01:36 +0000

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,12 +1,16 @@
 var firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyA_X3ZP2Shlqo7EuRyqX_AEBfYRk-G-Lyo",
+  authDomain: "lesse-motors.firebasestorage.com",
+  projectId: "lesse-motors",
+  storageBucket: "lesse-motors.firebasestorageapp.com",
+  messagingSenderId: "817526799996",
+  appId: "1:817526799996:web:2f2a478a9d515279dd13f9"
+  measurementId: "G-3VX67G8VWD"
+};
 };
 firebase.initializeApp(firebaseConfig);
 var auth = firebase.auth();
 var db = firebase.firestore();
 var storage = firebase.storage();
+
+  
